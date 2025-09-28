4036146d8560f02f377d8de689218cdc7cf07a4c
mudasirbasharat <gujjaarr776@gmail.com>
2025-09-29 02:16:56 +0500

    update

--- database/factories/TravelerFactory.php
+++ database/factories/TravelerFactory.php
@@ -18,17 +18,17 @@ class TravelerFactory extends Factory
     public function definition(): array
     {
         return [
-            'profile_photo' => $this->faker->imageUrl(200, 200, 'people'),
-            'name' => $this->faker->name(),
-            'username' => $this->faker->unique()->userName(),
-            'email' => $this->faker->unique()->safeEmail(),
+            'profile_photo' => fake()->imageUrl(200, 200, 'people'),
+            'name' => fake()->name(),
+            'username' => fake()->unique()->userName(),
+            'email' => fake()->unique()->safeEmail(),
             'password' => Hash::make('password'),
-            'phone' => $this->faker->phoneNumber(),
-            'country' => $this->faker->country(),
-            'address' => $this->faker->streetAddress(),
-            'spent_amount' => $this->faker->randomFloat(2, 0, 10000),
-            'status' => $this->faker->randomElement(['active', 'suspended']),
-            'last_active' => $this->faker->dateTimeBetween('-1 month', 'now'),
+            'phone' => fake()->phoneNumber(),
+            'country' => fake()->country(),
+            'address' => fake()->streetAddress(),
+            'spent_amount' => fake()->randomFloat(2, 0, 10000),
+            'status' => fake()->randomElement(['active', 'suspended']),
+            'last_active' => fake()->dateTimeBetween('-1 month', 'now'),
             'created_at' => now(),
             'updated_at' => now(),
         ];
